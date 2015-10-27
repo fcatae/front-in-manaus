6432c4bf3f59d002bd83658136fce3916764db54
Fabricio Catae <fabricio.catae@microsoft.com>
2015-10-27 08:38:56 -0200

    novas imagens

--- canvas.js
+++ canvas.js
@@ -8,57 +8,14 @@ img.onload = function() {
 	
 	context.fillRect(0,0,800,640);
 	
-	var stars = [];
-	
-	for(var i=0; i<5000; i++) {	
-		
+	for(var i=0; i<10000; i++) {	
 		var x = 800 * Math.random();
 		var y = 640 * Math.random();
 		var tam = 2 * Math.random();
 		
-		var obj = {
-			x: x,
-			y: y,
-			tam: tam
-		};
-		
-		stars[i] = obj;
-	}	
-		
-	setInterval(gameLoop, 13.3);
-	
-	function gameLoop() {
-	
-		context.fillRect(0,0,800,640);
-		
-		for(var i=0; i<5000; i++) {	
-			// update
-			stars[i].y = (stars[i].y + 1) % 640;
-			
-			// draw
-			context.drawImage(img, stars[i].x, stars[i].y, stars[i].tam, stars[i].tam);
-		}
+		context.drawImage(img, x, y, tam, tam);	
 	}
-	
-
 }
 
 
 
-
-
-
-
-
-var nave = document.getElementById('nave');
-
-nave.style.position = 'absolute';
-nave.style.left = '100px';
-nave.style.top = '100px';
-
-window.onmousemove = function(e) {
-	nave.style.cursor = 'none';
-	nave.style.position = 'absolute';
-	nave.style.left = -100 + e.clientX + 'px';
-	nave.style.top = -50 + e.clientY + 'px';
-}
--- game.js
+++ game.js
@@ -5,19 +5,26 @@ nave.style.left = '100px';
 nave.style.top = '100px';
 
 window.onmousemove = function(e) {
+	
 	nave.style.cursor = 'none';
 	nave.style.position = 'absolute';
-	nave.style.left = -100 + e.clientX + 'px';
+	nave.style.left = -50 + e.clientX + 'px';
 	nave.style.top = -50 + e.clientY + 'px';
+
 }
 
 window.onkeyup = function(e) {
-	var img = new Image();
-	img.src = "nave.png";
-
-	img.style.position = 'absolute';
-	img.style.left = nave.style.left;
-	img.style.top = nave.style.top;	
 	
-	document.body.appendChild(img);
+	var key = e.keyCode;
+	
+	if( key === 32 ) {
+		var img = new Image();
+		img.src = "nave.png";
+		
+		img.style.position = 'absolute';
+		img.style.left = nave.style.left;
+		img.style.top = nave.style.top;
+		
+		document.body.appendChild(img);
+	}
 }
